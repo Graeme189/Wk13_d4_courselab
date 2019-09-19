78cbe1b368c63ec222b3942122b7c3525cd2add2
Graeme Henderson <graeme.189@gmail.com>
2019-09-19 11:10:46 +0100

    findcoursebyrating method added & tests complete

--- src/main/java/com/codeclan/codingcourselab/CodingCourseLab/repositories/CourseRepository/CourseRepository.java
+++ src/main/java/com/codeclan/codingcourselab/CodingCourseLab/repositories/CourseRepository/CourseRepository.java
@@ -3,5 +3,9 @@ package com.codeclan.codingcourselab.CodingCourseLab.repositories.CourseReposito
 import com.codeclan.codingcourselab.CodingCourseLab.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long>, CourseRepositoryCustom {
+
+    List<Course> findCoursesByRating(int rating);
 }
--- src/test/java/com/codeclan/codingcourselab/CodingCourseLab/CodingCourseLabApplicationTests.java
+++ src/test/java/com/codeclan/codingcourselab/CodingCourseLab/CodingCourseLabApplicationTests.java
@@ -1,16 +1,40 @@
 package com.codeclan.codingcourselab.CodingCourseLab;
 
+import com.codeclan.codingcourselab.CodingCourseLab.models.Course;
+import com.codeclan.codingcourselab.CodingCourseLab.repositories.BookingRepository.BookingRepository;
+import com.codeclan.codingcourselab.CodingCourseLab.repositories.CourseRepository.CourseRepository;
+import com.codeclan.codingcourselab.CodingCourseLab.repositories.CustomerRepository.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CodingCourseLabApplicationTests {
 
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void canFindCourseByRating() {
+		List<Course> found = courseRepository.findCoursesByRating(3);
+		assertEquals("Python", found.get(0).getName());
 	}
 
 }
